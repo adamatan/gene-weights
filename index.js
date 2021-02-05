9556aac9507b2c670cf882db0432718d319620a4
Adam Matan <adam@matan.name>
2021-02-05 21:47:35 +0200

    WIP

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // const gene = 'GAGCAATTTCTGAAGGTTTCAAAGAAGTGAACGTTGTTGGGCACAACGACGGGAATTCCTTGTG';
 const gene = 'GAGCA';
 
-weights = {
-    'A': 1,
-    'C': 2,
-    'G': 3,
-    'T': 4
+const weights = {
+    'A': 314.21,
+    'C': 289.18,
+    'G': 329.21,
+    'T': 304.19
 }
 
 function gene_weight(gene){
-    weight = 0;
+    let weight = 0;
     for (let i=0; i<gene.length; i++){
         weight += weights[gene.charAt(i)]
     }
-    return weight
+    return weight - 62.97 + 1.008
 }
 
 for (let i=0; i<gene.length; i++) {
     for (let j=gene.length; j>i; j--) {
-        sub_gene = gene.slice(i, j)
-        prefix_padding = '.'.repeat(i)
-        suffix_padding = '.'.repeat(gene.length-j)
+        let sub_gene = gene.slice(i, j)
+        let prefix_padding = '.'.repeat(i)
+        let  suffix_padding = '.'.repeat(gene.length-j)
         console.log(`${prefix_padding}${sub_gene}${suffix_padding}  ${gene_weight(sub_gene)}`)
     }
 }
