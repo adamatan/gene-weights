bac6b68b7b0e82bfe38b50fa7c324edd46cb3cc1
Adam Matan <adam@matan.name>
2021-02-05 22:24:00 +0200

    WIP

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 // const gene = 'GAGCAATTTCTGAAGGTTTCAAAGAAGTGAACGTTGTTGGGCACAACGACGGGAATTCCTTGTG';
-const gene = 'GAGCA';
+const gene = 'gaGCA';
 
 const weights = {
-    'A': 314.21,
+    'A': 313.21,
     'C': 289.18,
     'G': 329.21,
-    'T': 304.19
+    'T': 304.19,
+    'a': 313.21 + 16.061,
+    'c': 289.18 + 16.061,
+    'g': 329.21 + 16.061,
+    't': 304.19 + 16.061
 }
 
-function gene_weight(gene){
+const masses = [100, 300, 600, 1000, 1500, 1550, 2000]
+
+function find_closest_mass(mass){
+    closest_mass = masses[0]
+    for (let i=0; i<masses.length; i++){
+        if (Math.abs(masses[i]-mass) < Math.abs(closest_mass-mass)) {
+            closest_mass = masses[i];
+        }
+    }
+    return closest_mass
+}
+
+function mass_differences(gene_weight) {
+    return masses.map(i => gene_weight-i)
+}
+
+function calculate_gene_weight(gene){
     let weight = 0;
     for (let i=0; i<gene.length; i++){
         weight += weights[gene.charAt(i)]
@@ -16,11 +36,28 @@ function gene_weight(gene){
     return weight - 62.97 + 1.008
 }
 
+function print_titles(){
+    let titles = ['Gene', 'Left (5)', 'Right(3)', 'Mass']
+    for (let i=0; i<masses.length; i++) {
+        titles.push(masses[i])
+    }
+    console.log(titles.join(','))
+}
+
+print_titles()
 for (let i=0; i<gene.length; i++) {
     for (let j=gene.length; j>i; j--) {
         let sub_gene = gene.slice(i, j)
-        let prefix_padding = '.'.repeat(i)
-        let  suffix_padding = '.'.repeat(gene.length-j)
-        console.log(`${prefix_padding}${sub_gene}${suffix_padding}  ${gene_weight(sub_gene)}`)
+        let right_chop = i
+        let left_chop = gene.length-j
+        let prefix_padding = '.'.repeat(right_chop)
+        let suffix_padding = '.'.repeat(left_chop)
+        let gene_weight = calculate_gene_weight(sub_gene)
+        let closest_mass = find_closest_mass(gene_weight);
+        let mass_difference = closest_mass - gene_weight;
+        let output = `${prefix_padding}${sub_gene}${suffix_padding} ,` +
+            `${right_chop}, ${left_chop}, ${gene_weight}` +
+            `${closest_mass}, ${mass_differences(gene_weight)}`
+        console.log(output)
     }
 }
