21c1f3ae5ef1d7bc539094403dde635daf8dca60
Adam Matan <adam@matan.name>
2021-02-06 16:24:19 +0200

    Start parsing the gene

--- docs/script.js
+++ docs/script.js
@@ -4,14 +4,25 @@ function getGeneValues() {
   values = []
   totalMass = document.getElementById("total-mass");
   totalMass.innerHTML = '';
-  for (let i=0; i<genes.length; i++){ 
-    let number=document.getElementById(`gene-${genes[i]}`).value;  
+  for (let i=0; i<genes.length; i++){
+    let number=document.getElementById(`gene-${genes[i]}`).value;
     values.push(number)
-    totalMass.innerHTML += (number.toString() + '<br>');    
+    totalMass.innerHTML += (number.toString() + '<br>');
   }
   console.log(values);
+  geneText = document.getElementById('geneText').value;
+  console.log(geneCombinations(geneText))
 }
 
 document.querySelectorAll('.gene-numeric-input').forEach(item => {
   item.addEventListener('change', getGeneValues);
 })
+
+
+function geneCombinations(geneText) {
+  subGenes = []
+  for (let i=0; i<geneText.length; i++) {
+    subGenes.push(geneText.slice(0, i))
+  }
+  return subGenes;
+}
